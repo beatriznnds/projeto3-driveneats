5e2d9257b7afcc68181caf469514cd18b81b3510
beatriz <beatriz.nnds@gmail.com>
2022-04-02 00:41:13 -0300

    git commit

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let comidaSelecionada;
 let bebidaSelecionada;
 let sobremesaSelecionada;
 let vetor;
+let precoComida;
+let precoBebida;
+let precoSobremesa;
 
 
 function selecionarComida(comida) {    
@@ -13,7 +16,8 @@ function selecionarComida(comida) {
      comidaSelecionada.classList.toggle("prato-selecionado");
      comidaSelecionada.querySelector("svg").classList.toggle("vetor-selecionado");     
  }
- comida.classList.add("prato-selecionado", "vetor-funcionando")
+ comida.classList.add("prato-selecionado", "vetor-funcionando");
+ precoComida = Number(comida.document.querySelector(".produto-individual h5"));
 }
 
 function selecionarBebida(bebida) {
@@ -26,6 +30,7 @@ function selecionarBebida(bebida) {
         bebidaSelecionada.querySelector("svg").classList.toggle("vetor-selecionado");
     }
     bebida.classList.add("prato-selecionado", "vetor-funcionando");
+    precoBebida = Number(bebida.document.querySelector(".produto-individual h5"));
 }
 
 function selecionarSobremesa(sobremesa) {
@@ -38,10 +43,22 @@ function selecionarSobremesa(sobremesa) {
         sobremesaSelecionada.querySelector("svg").classList.toggle("vetor-selecionado");
     }
     sobremesa.classList.add("prato-selecionado", "vetor-funcionando");
+    precoSobremesa = Number(sobremesa.document.querySelector(".produto-individual h5"));
 }
 
 function finalizarPedido () {
-    if (comida && bebida && sobremesa) {
-        document.querySelector("button").classList.remove("fechar-pedido")
+    if (comidaSelecionada && bebidaSelecionada && sobremesaSelecionada) {
+        document.querySelector("button").classList.add("botao-selecionado")
+        document.querySelector(".mensagem-pedido").innerHTML = `Fechar pedido`;    
     }
+    let nome = prompt("Qual é seu nome?");
+    let endereco = prompt("Qual é o seu endereço?");
+    let str = `Olá, gostaria de fazer o pedido:
+    - Prato: ${comidaSelecionada}
+    - Bebida: ${bebidaSelecionada}
+    - Sobremesa: ${sobremesaSelecionada}
+    Total: ${precoComida + precoBebida + precoSobremesa}
+    Nome: ${nome}
+    Endereço: ${endereco}`;
+    window.open("https://wa.me/55219631235636?=text" + encondeURIComponent(str));
 }
